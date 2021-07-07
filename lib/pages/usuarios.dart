922a9c0c5bfd5ecfa82e0c2de3e8f18618286717
Ricardo Morales Domínguez <rmorales@metrofflex.com>
2021-07-06 19:58:22 -0500

    pull to refresh on users page

--- lib/pages/usuarios.dart
+++ lib/pages/usuarios.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/models/usuario.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class UsuariosPage extends StatefulWidget {
   @override
@@ -7,6 +8,7 @@ class UsuariosPage extends StatefulWidget {
 }
 
 class _UsuariosPageState extends State<UsuariosPage> {
+  final RefreshController _refreshController = RefreshController();
   final usuarios = [
     Usuario(uid: '1', nombre: 'Ricardo', email: 'test@test.com', online: true),
     Usuario(uid: '2', nombre: 'Morales', email: 'test@test.com', online: true),
@@ -16,47 +18,85 @@ class _UsuariosPageState extends State<UsuariosPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Mi nombre',
-            style: TextStyle(color: Colors.black87),
-          ),
-          elevation: 1,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: const Icon(
-              Icons.exit_to_app,
-              color: Colors.black87,
-            ),
-            onPressed: () {},
-          ),
-          actions: [
-            Container(
-              margin: const EdgeInsets.only(right: 10),
-              child: const Icon(
-                Icons.check_circle,
-                color: Colors.red,
-              ),
-            )
-          ],
+      appBar: AppBar(
+        title: const Text(
+          'Mi nombre',
+          style: TextStyle(color: Colors.black87),
         ),
-        body: ListView.separated(
-          physics: BouncingScrollPhysics(),
-          itemBuilder: (_, i) => ListTile(
-            title: Text(usuarios[i].nombre),
-            leading: CircleAvatar(
-              child: Text(usuarios[i].nombre.substring(0, 2)),
-            ),
-            trailing: Container(
-              width: 10,
-              height: 10,
-              decoration: BoxDecoration(
-                  color: usuarios[i].online ? Colors.green[300] : Colors.red,
-                  borderRadius: BorderRadius.circular(100)),
-            ),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.exit_to_app,
+            color: Colors.black87,
           ),
-          separatorBuilder: (_, i) => Divider(),
-          itemCount: usuarios.length,
-        ));
+          onPressed: () {},
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: const Icon(
+              Icons.check_circle,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        onRefresh: _cargarUsuario,
+        header: WaterDropHeader(
+          complete: Icon(
+            Icons.check,
+            color: Colors.blue[400],
+          ),
+          waterDropColor: Colors.blue[400],
+        ),
+        //enablePullDown: true,
+        child: _listViewUsuario(),
+      ),
+    );
+  }
+
+  ListView _listViewUsuario() {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, i) => _UserView(usuario: usuarios[i]),
+      separatorBuilder: (_, i) => const Divider(),
+      itemCount: usuarios.length,
+    );
+  }
+
+  _cargarUsuario() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    _refreshController.refreshCompleted();
+  }
+}
+
+class _UserView extends StatelessWidget {
+  const _UserView({
+    Key key,
+    @required this.usuario,
+  }) : super(key: key);
+
+  final Usuario usuario;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(usuario.nombre),
+      subtitle: Text(usuario.email),
+      leading: CircleAvatar(
+        child: Text(usuario.nombre.substring(0, 2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            color: usuario.online ? Colors.green[300] : Colors.red,
+            borderRadius: BorderRadius.circular(100)),
+      ),
+    );
   }
 }
